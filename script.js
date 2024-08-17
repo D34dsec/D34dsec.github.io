b109205201b0ebc9ff2411e37b05b7de5c3c33ea
D34dsec <james.sawkins@googlemail.com>
2024-08-18 00:32:03 +0100

    Added a carosel for the service as more will be added later

--- script.js
+++ script.js
@@ -9,3 +9,33 @@ function scrollToTop() {
     window.scrollTo({ top: 0, behavior: 'smooth' });
 }
 
+$(document).ready(function(){
+    $('.service-carousel').slick({
+      slidesToShow: 3, // Show 3 cards at once on large screens
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 5000,
+      dots: true,
+      arrows: true,
+      adaptiveHeight: true, // Enable adaptive height
+      responsive: [
+        {
+          breakpoint: 1024, // Show 2 cards on medium screens
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 768, // Show 1 card on small screens
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+    });
+  });
+  
+  
+  
